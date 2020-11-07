a8fdb089a5043d9daa37db63a761229db2a9354d
bhild <bryanhildebrand1@gmail.com>
2020-11-07 08:01:03 -0800

    added a GUI framework

--- CutieHack/src/Driver.java
+++ CutieHack/src/Driver.java
@@ -1,4 +1,6 @@
 
 public class Driver {
-
+	public static void main(String[] args) {
+		new GUI();
+	}
 }
---
+++ CutieHack/src/GUI.java
@@ -0,0 +1,57 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+
+public class GUI extends JFrame implements KeyListener{
+	private static final int CANVAS_WIDTH = 100;
+	private static final int CANVAS_HEIGHT = 100;
+	private DrawCanvas canvas;
+	public GUI() {
+		canvas = new DrawCanvas();   
+		canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
+		Container cp = getContentPane();
+		cp.add(canvas);
+		setDefaultCloseOperation(EXIT_ON_CLOSE); 
+		pack();  
+		setTitle("Test");
+		setVisible(true);
+	}
+	private class DrawCanvas extends JPanel {
+		@Override
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);  
+			for (int i = 0; i < 100; i++) {
+				for (int j = 0; j < 10; j++) {
+					g.drawRect(i*20, j*40, 20, 40);
+				}
+			}
+			g.setColor(Color.blue);
+		}
+	}
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
